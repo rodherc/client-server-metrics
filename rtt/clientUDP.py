5af376d9d296c27723bd3f9453b2a7bbdb1e05de
willdc4 <williamdavicoelho@gmail.com>
2019-09-04 13:53:04 -0300

    calculo de vazao adicionado

--- rtt/clientUDP.py
+++ rtt/clientUDP.py
@@ -2,6 +2,9 @@ import socket
 import os
 import time
 
+def to_miliseconds(seconds): # Transforma segundos em milisegundos
+	return round(seconds * 1000,4) # Arredondamento com 4 casas
+
 udp_ip = "127.0.0.1"
 udp_port = 3003
 
@@ -16,14 +19,16 @@ sock.settimeout(0.1)
 # Faz o bind local. Associa um socket com um IP e uma Porta.
 sock.bind((udp_ip, udp_port))
 soma_rtt = 0
+num_pacotes_total = 20
 num_pacotes_perdidos = 0
 
-for i in range(20):
-	sendMessage = "Mensagem " + str(i)	
+for i in range(num_pacotes_total):
+	send_message = "Mensagem " + str(i)	
 	start = time.time() # Momento em que a mensagem e enviada
-	sock.sendto(sendMessage, (udp_ip_send, udp_port_send))
+	sock.sendto(send_message, (udp_ip_send, udp_port_send))
+	
 	try:
-		recvMessage, addr = sock.recvfrom(1024) # Tamanho do buffer eh 1024 bytes
+		recv_message, addr = sock.recvfrom(1024) # Tamanho do buffer eh 1024 bytes
 	except socket.timeout: # Nao recebeu a resposta no tempo limite
 		num_pacotes_perdidos += 1
 		print "Timeout\n"
@@ -32,13 +37,16 @@ for i in range(20):
 	
 	end = time.time()   # Momento em que a resposta do server e recebida
 	
-	if (recvMessage != ''):
-		rtt = round(((end - start) * 1000), 4)
+	if (recv_message != ''):
+		rtt = end - start
 		soma_rtt += rtt
-		print recvMessage + ", rtt: " + str(rtt) + "ms" 
+		print recv_message + ", ping: " + str(to_miliseconds(rtt)) + " ms" 
 	#time.sleep(1)
 
-print "Latencia (RTT) medio: "+ str(soma_rtt/20) + "ms"
-print str(num_pacotes_perdidos) + " numero de pacotes perdidos"
-#print "Vazao:"
-sock.close()	
+num_pacotes_enviados = num_pacotes_total - num_pacotes_perdidos
+rtt_medio = soma_rtt/num_pacotes_enviados
+
+print "Latencia (RTT) medio: "+ str(to_miliseconds(rtt_medio)) + " ms"
+print "Vazao: " + str( round(len(send_message.encode("utf8")) / (rtt_medio * 1000))) + " kb/s"
+print str(num_pacotes_perdidos) + " pacotes perdidos"
+sock.close()
--- rtt/serverUDP.py
+++ rtt/serverUDP.py
@@ -7,20 +7,20 @@ udp_port = 4004
 #udp_ip_send = "127.0.0.1"
 udp_port_send = 3003
 
-sockS = socket.socket(socket.AF_INET, # Internet
+sock_server = socket.socket(socket.AF_INET, # Internet
                     socket.SOCK_DGRAM) # UDP
 					
-sockC = socket.socket(socket.AF_INET, # Internet
+sock_client = socket.socket(socket.AF_INET, # Internet
                     socket.SOCK_DGRAM) # UDP
                      
 # Faz o bind local. Associa um socket com um IP e uma Porta.
-sockS.bind((udp_ip, udp_port))
+sock_server.bind((udp_ip, udp_port))
 
 while True:
 	message, addr = sockS.recvfrom(1024) # Tamanho do buffer eh 1024 bytes
 	print message
-	sockC.sendto(message + " foi recebida", (addr[0], udp_port_send)) # Resposta do server para calcular latencia (RTT)
+	sock_client.sendto(message + " foi recebida", (addr[0], udp_port_send)) # Resposta do server para calcular latencia (RTT)
 
-sockC.close()
-sockS.close()
+sock_client.close()
+sock_server.close()
 
