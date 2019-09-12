6593bf4b31b3c3c8391719f0673125684b561c74
rodherc <rodrigo.herculano@estudante.ufla.br>
2019-09-12 15:39:29 -0300

    enviar arquivo ainda n funcionando

--- tcp/clientTCP.py
+++ tcp/clientTCP.py
@@ -1,21 +1,53 @@
 #!/usr/bin/env python
 
 import socket
+import time
 
-tcp_ip = '127.0.0.1'
+
+def to_miliseconds(seconds): # Transforma segundos em milisegundos
+	return round(seconds * 1000,4) # Arredondamento com 4 casas
+	
+CHUNK_SIZE = 10 * 1024
+
+tcp_ip = '192.168.105.54'
 tcp_port = 5006
 
 s = socket.socket(socket.AF_INET,
 				socket.SOCK_STREAM)
 s.connect((tcp_ip, tcp_port))
 
-while True:
-	#message = raw_input("Digite Sua Mensagem: ")
-	arquivo = open("entrada.txt") 
-	message = arquivo.read()
-	s.send(message)
+arquivo = open("entrada.txt") 
+cont = 0
 
-	data = s.recv(1024) #Tamanho do buffer.
+
+message = arquivo.read(CHUNK_SIZE)
+
+
+start = time.time() # Momento em que a mensagem e enviada
+tam = 0
+while message:
+	s.send(message)
+	
+	data = s.recv(CHUNK_SIZE) #Tamanho do buffer.
+	tam = tam + len(data)
+	cont = cont + 1
+	print cont
 	print "Mensagem recebida: ", data
+	
+	message = arquivo.read(CHUNK_SIZE)
+	
+end = time.time()   # Momento em que a resposta do server e recebida
+rtt = end - start
+
+arquivo.close()
+arquivo = open("entrada.txt")
+
+tamMessage = arquivo.read()
+tamMessage = len(tamMessage)
+arquivo.close()
+print "Tamanho enviado e recebido :" , tam 
+print "Tamanho Mensagem :", tamMessage
+print "Latencia (RTT): "+ str(to_miliseconds(rtt)) + " ms"
+print "Vazao :" + str(round((tam/rtt) /  1048576,4)) + " MB/s"
 
 s.close()
--- tcp/serverTCP.py
+++ tcp/serverTCP.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import socket
+import time
+
+CHUNK_SIZE = 10 * 1024
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5006
@@ -9,16 +12,22 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
  
-conn, addr = s.accept()
+
 #print 'Endereco de conexao: ', addr
-cont = 0
 while True:
-	data = conn.recv(9216) #Tamanho do buffer.
-	cont = cont + 1
-	print cont
-	print "Mensagem Recebida:", data
-	message = data.upper()
-	conn.send(message)  # echo
-
-
-conn.close()
+	erro = False
+	conn, addr = s.accept()
+	data = conn.recv(CHUNK_SIZE) #Tamanho do buffer.
+	cont = 0
+	while (data and not erro):
+		cont = cont + 1
+		print cont
+		print "Mensagem Recebida:", data
+		#message = data.upper()
+		try:
+			conn.send(data)  # echo
+			data = conn.recv(CHUNK_SIZE) #Tamanho do buffer.
+		except socket.error, ex:
+			print "ERRO"
+			erro = True
+	conn.close()
