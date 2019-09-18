84574454629695b1377f7d84d7e4cc3494cd4150
rodherc <rodrigo.herculano@estudante.ufla.br>
2019-09-18 19:01:54 -0300

    % pacotes perdidos

--- tcp/clientTCP.py
+++ tcp/clientTCP.py
@@ -4,7 +4,7 @@ import socket
 import time
 import os
 
-TCP_IP = '192.168.103.8'
+TCP_IP = '192.168.104.13'
 TCP_PORT = 5006
 
 sock = socket.socket(socket.AF_INET,
@@ -23,7 +23,7 @@ end = time.time()   # Momento em que a resposta do server e recebida
 rtt = end - start
 
 tam = os.path.getsize('entrada.txt')
-
+print "\n               --- Estatisticas --- \n"
 print "Tamanho Mensagem:",tam, "bytes"
 print "Latencia (RTT): ", round(rtt * 1000,4), "ms"
 print "Vazao:", round((8* tam/rtt) /1000000, 4), "Mb/s"
--- udp/clientUDP.py
+++ udp/clientUDP.py
@@ -3,13 +3,13 @@ import os
 import time
 
 #Dados server
-UDP_IP_SEND = "192.168.103.8"
-UDP_PORT_SEND = 4004
+UDP_IP_SEND = "192.168.104.13"
+UDP_PORT_SEND = 3008
 
 sock_udp = socket.socket(socket.AF_INET, # Internet 
 				 	socket.SOCK_DGRAM) # UDP
 
-sock_udp.settimeout(0.1)
+sock_udp.settimeout(0.001)
 
 sum_rtt = 0
 num_pacotes_total = 20
@@ -17,7 +17,7 @@ num_pacotes_perdidos = 0
 
 for i in range(num_pacotes_total):
 	send_message = "Mensagem " + str(i)
-	time.sleep(0.250)	
+	#time.sleep(0.250)	
 	start = time.time() # Momento em que a mensagem e enviada
 	sock_udp.sendto(send_message, (UDP_IP_SEND, UDP_PORT_SEND))
 	
@@ -36,8 +36,13 @@ for i in range(num_pacotes_total):
 		#print "ping:", round(rtt * 1000,4), " ms" 
 
 num_pacotes_enviados = num_pacotes_total - num_pacotes_perdidos
+print "\n               --- Estatisticas --- \n"
+print num_pacotes_total, "pacotes transmitidos, ",num_pacotes_enviados , "pacotes recebidos, ",num_pacotes_perdidos/ num_pacotes_total * 100 ,"% pacotes perdidos." 
 if num_pacotes_enviados != 0:
 	rtt_medio = sum_rtt/num_pacotes_enviados
-	print "Latencia (RTT) medio:", round(rtt_medio * 1000,4), "ms"
-print num_pacotes_perdidos, "pacotes perdidos"
+
+	print "Latencia (RTT) medio:", round(rtt_medio * 1000,4), "ms \n"
+
+
+
 sock_udp.close()
